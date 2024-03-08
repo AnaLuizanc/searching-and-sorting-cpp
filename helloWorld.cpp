e958a83fab57b5865af474c086ae648dac3d38be
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-08 20:08:09 -0300

    delete helloWorld

--- helloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-using namespace std;
-
-int main(){
-    cout <<"Hello, world!"<< endl;
-    cout <<"Hello, I'm Ana Luiza and I starting my studies in Searching and Sorting Techiniques with C++";
-    return 0;
-}
