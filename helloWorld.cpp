011d69a3070a942c027d73fb496f23774cf24899
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-08 20:02:05 -0300

    error correction

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main(){
     cout <<"Hello, world!"<< endl;
-    cout <<"Hello, I'm Ana Luiza and I starting my studies in Searching and Sorting Techiniques with C/++,"
+    cout <<"Hello, I'm Ana Luiza and I starting my studies in Searching and Sorting Techiniques with C++";
     return 0;
 }
