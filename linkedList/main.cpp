b6c0c5b56948466c12c0852db318ee21b6c2e817
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-10 17:14:21 -0300

    change Vector class

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -5,13 +5,19 @@ using namespace std;
 
 class Vector{
     public:
-        int valor;
-        int qtdAcessos;
         int lista[N_MAX];
+        int tamanho;
+        int qtdAcessos;
+        
+
+    
 };
 
 int main(){
     Vector list;
+    list.qtdAcessos=0;
+    int n;
+    cin >> n;
     
     return 0;  
 }
