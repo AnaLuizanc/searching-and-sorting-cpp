5f1646c08d52818881a7ed8991c33dc663b0b693
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-10 16:59:38 -0300

    add Vector class

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
+#include<string>
+#define N_MAX 100
 using namespace std;
 
+class Vector{
+    public:
+        int valor;
+        int qtdAcessos;
+        int lista[N_MAX];
+};
+
 int main(){
-    cout <<"Testing" << endl;
-    return 0;
+    Vector list;
+    
+    return 0;  
 }
