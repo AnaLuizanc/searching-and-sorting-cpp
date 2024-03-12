b56ca606586d7d97745dc5c3e5e709e051d89bda
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-12 19:33:17 -0300

    replace class with struct

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -7,32 +7,16 @@
 using namespace std;
 #include "list.h"
 
-class SimpleNode{
-    public:
-        SimpleNode(int valor){
-            this->valor = valor;
-            this->prox = NULL;
-        }
-        ~SimpleNode(){}
+typedef struct node_aux{
+    int valor;
+    int qtdAcessos;
+    int tamanho;
+    struct node_aux* prox;
+}Node;
 
-        int valor;
-        SimpleNode* prox;
-};
+typedef Node* List;
 
-class LinkedList{
-    public:        
-        LinkedList(){
-            qtdAcessos = 0;
-            tamanho = 0;
-            cabeca = NULL;
-        }
-        ~LinkedList(){}
 
-        int valor;
-        int qtdAcessos;
-        int tamanho;
-        SimpleNode* cabeca;
-};
 
 int main(){
 
