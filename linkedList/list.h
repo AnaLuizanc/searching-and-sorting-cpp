e290b40ac5a0e40669c390e3053add2b3ddd890b
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-11 21:51:35 -0300

    move StaticList class from main.cpp to list.h

--- linkedList/list.h
+++ linkedList/list.h
@@ -1,6 +1,101 @@
 #ifndef LIST_H_INCLUDED
 #define LIST_H_INCLUDED
 
+class StaticList{
+    public:
+        StaticList(){
+            qtdAcessos = 0;
+            tamanho = 0;
+        }
+        ~StaticList(){}
+        
+        int vector[N_MAX];
+        int qtdAcessos;
+        int tamanho;
 
+        void armazenaValores(int n){
+            for(int i=0; i<n; i++)
+                cin >> vector[i];
+            tamanho+=n;
+            qtdAcessos+=n;
+        }
+
+        void imprime(){
+            for(int i=0; i<tamanho; i++){
+                cout << vector[i] << " ";
+                qtdAcessos++;
+            }
+            cout << endl;
+        }
+
+        void insereFim(int valor){
+            vector[tamanho] = valor;
+        }
+        
+        int andaUm(int posicao){
+            int posicaoAux = tamanho;
+            while(posicaoAux != posicao){
+                vector[posicaoAux] = vector[posicaoAux-1];
+                posicaoAux--;
+            }
+            return posicao;
+        }
+
+        void inserePosicao(int valor, int posicao){
+            if(posicao == tamanho){
+                insereFim(valor);
+                tamanho++;
+                qtdAcessos++;
+            }
+            else{
+                posicao = andaUm(posicao);
+                vector[posicao] = valor;
+                tamanho++;
+                qtdAcessos++;
+            }
+        }
+
+        void trocaPosicoes(int posicaoA, int posicaoB){
+            int auxiliar;
+            auxiliar = vector[posicaoA];
+            vector[posicaoA] = vector[posicaoB];
+            vector[posicaoB] = auxiliar;
+            qtdAcessos++;
+        }
+
+        int menorValor(){
+            int menor = vector[0];
+            int posicao = 0;
+            for(int i=1; i<tamanho; i++){
+                if(menor > vector[i]){
+                    menor = vector[i];
+                    posicao = i;
+                    qtdAcessos++;
+                }
+                qtdAcessos++;    
+            }
+            return posicao;
+        }
+
+        void geraListaAleatoria(int size, int inicio, int fim){
+            srand(time(0));
+            for(int i=0; i<size; i++)
+                vector[i] = rand() % (fim-inicio+1) + inicio;
+            for(int i=0; i<size; i++)
+                cout << vector[i] << " ";
+            cout << endl;
+        }
+
+        int buscaValor(int valor){
+            for(int i=0; i<tamanho; i++){
+                if(vector[i] == valor){
+                    qtdAcessos++;
+                    return i;
+                }
+                qtdAcessos++;    
+            }
+            return -1;
+        }
+};
 
 #endif
