945c84782c25d46a1d5c3b52b7b640acec064dab
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-13 08:43:39 -0300

    add struct DoubleList

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -7,6 +7,16 @@
 using namespace std;
 #include "list.h"
 
+typedef struct no_aux{
+    int valor;
+    int qtdAcessos; 
+    int tamanho;
+    struct no_aux* prox;
+    struct no_aux* antec;
+}DoubleNode;
+
+typedef DoubleNode* ListD;
+
 int main(){
     
     return 0;  
