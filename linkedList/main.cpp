1a95cb421715df747ba147c47f13a2853223adbc
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-13 08:37:55 -0300

    clean main function

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -7,30 +7,7 @@
 using namespace std;
 #include "list.h"
 
-
-void imprimeInverso(List lista){
-
-}
-
 int main(){
-    List lista;
-    /*inicializaLista(&lista);
-    int qtd, valor;
-    //cin >> valor;
-    armazenaValores(&lista, 56);
-    armazenaValores(&lista, 47);
-    armazenaValores(&lista, 77);
-    armazenaValores(&lista, 13);
-    armazenaValores(&lista, 54);
-    imprime(lista);
-    swap(&lista, 1, 3);
-    imprime(lista);
-    cout << "Posicao: " << buscaValor(lista, 77) << endl;
-    cout << "Posicao: " << buscaValor(lista, 11) << endl;
-    List menor = buscaMenor(lista);
-    cout << "Menor: " << menor->valor << endl;
-    imprimeInverso(lista);
-    cout << lista->tamanho;
-*/
+    
     return 0;  
 }
