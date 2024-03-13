9dac45c4d7f16bcef4cf00a7b0f6ffb3c3813437
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-13 20:12:41 -0300

    clean main function

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -8,21 +8,7 @@ using namespace std;
 #include "list.h"
 
 int main(){
-    SimpleList l;
-    l.armazenaValores(7);
-    l.armazenaValores(65);
-    l.imprime();
-    l.inserePosicao(4, 100);
-    l.inserePosicao(0, 90);
-    l.imprime();
-    l.inserePosicao(3, 3);
-    //l.swap(1, 2);
-    l.imprime();
-
-    SimpleNode* menor = l.buscaMenor();
-    cout << "Menor: " << menor->valor << endl;
-
-    cout << l.buscaValor(65) << " " << l.buscaValor(2) << endl;
+    
     
     return 0;  
 }
