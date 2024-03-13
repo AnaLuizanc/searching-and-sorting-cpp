1bfef3823f5dc285e128369d70e2fbe05bbe4d8b
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-12 22:36:18 -0300

    change inicializaLista function

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -17,8 +17,10 @@ typedef struct node_aux{
 typedef Node* List;
 
 void inicializaLista(List* apLista){
-    Node* cabeca = new Node;
-    cabeca->qtdAcessos = cabeca->tamanho = 0;
+    Node* cabeca = (Node*)malloc(sizeof(Node));
+    cabeca->qtdAcessos = 0;
+    cabeca->tamanho = 0;
+    cabeca->valor = -1;
     cabeca->prox = NULL;
     *apLista = cabeca;      
 }
@@ -29,12 +31,6 @@ int main(){
     List lista;
     inicializaLista(&lista);
     int qtd, valor;
-    cin >> qtd;
-   // for(int i=0; i<qtd; i++){
-        cin >> valor;
-        armazenaValores(&lista, valor);
-    //}
-    imprime(lista);
 
     return 0;  
 }
