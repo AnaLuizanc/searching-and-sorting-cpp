ad5da09110957bdb71e9330a25ad0abd8e839f2f
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-13 21:28:13 -0300

    change DoubleList class

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -19,53 +19,25 @@ class DoubleList{
         DoubleList(){
             tamanho = 0;
             qtdAcessos = 0;
-            cabeca = NULL;
-            cauda = NULL;
+            cabeca->prox = cauda;
+            cauda->antec = cabeca;
         }
 
         int tamanho;
         int qtdAcessos;
-        DoubleNode* cabeca;
-        DoubleNode* cauda;
+        DoubleNode* cabeca = new DoubleNode;
+        DoubleNode* cauda = new DoubleNode;
 
-        void armazenaValores(int valor){
-            if(tamanho == 0){
-                DoubleNode* novo = new DoubleNode;
-                novo->valor = valor;
-                novo->prox = NULL;
-                novo->antec = NULL;
-                cabeca = novo;
-                cauda = novo;
-                qtdAcessos+=2;
-                tamanho++;
-            }else{
-                DoubleNode* ultimo = cauda->antec;
-                DoubleNode* novo = new DoubleNode;
-                novo->valor = valor;
-                novo->prox = cauda;
-                cauda->antec = novo;
-                novo->antec = ultimo;
-                ultimo->prox = novo;
-                qtdAcessos+=5;
-                tamanho++;
-            }
-        }
-
-        void imprime(){
-            DoubleNode* atual = cabeca;
-            qtdAcessos++;
-            while(atual != NULL){
-                cout << atual->valor << " ";
-                atual = atual->prox;
-                qtdAcessos+=2;
-            }
-            cout << endl;
-        }
+        
 };
 
 int main(){
     DoubleList l;
     l.armazenaValores(12);
+    l.armazenaValores(45);
+    l.armazenaValores(100);
+    l.armazenaValores(147);
+    cout << l.tamanho << endl;
     l.imprime();
     
     return 0;  
