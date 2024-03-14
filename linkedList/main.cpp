ee6b5b8aeba08b529ca066c23fe0ca04393a0b8a
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-13 21:55:17 -0300

    add inserePosicao function

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -36,7 +36,7 @@ class DoubleList{
                 novo->valor = valor;
                 novo->prox = cauda;
                 novo->antec = cabeca;
-                qtdAcessos+=2;
+                qtdAcessos+=4;
                 tamanho++;
             }else{
                 DoubleNode* ultimo = cauda->antec;
@@ -47,6 +47,54 @@ class DoubleList{
                 cauda->antec = novo;
                 ultimo->prox = novo;
                 qtdAcessos+=5;
+                tamanho++;
+            }
+        }
+
+        void inserePosicao(int valor, int posicao){
+            if(tamanho == 0){
+                DoubleNode* novo = new DoubleNode;
+                cabeca->prox = novo;
+                cauda->antec = novo;
+                novo->valor = valor;
+                novo->prox = cauda;
+                novo->antec = cabeca;
+                qtdAcessos+=4;
+                tamanho++;
+            }else if(posicao == 0){
+                DoubleNode* novo = new DoubleNode;
+                novo->valor = valor;
+                cabeca->prox->antec = novo;
+                novo->prox = cabeca->prox;
+                cabeca->prox = novo;
+                novo->antec = cabeca;
+                qtdAcessos+=6;
+                tamanho++;
+            }else if(posicao >= tamanho){
+                DoubleNode* novo = new DoubleNode;
+                novo->valor = valor;
+                cauda->antec->prox = novo;
+                novo->antec = cauda->antec;
+                novo->prox = cauda;
+                cauda->antec = novo;
+                qtdAcessos+=6;
+                tamanho++;
+            }else{
+                DoubleNode* anterior = cabeca;
+                DoubleNode* seguinte = cabeca->prox;
+                qtdAcessos+=2;
+                for(int i=0; i<=posicao; i++){
+                    anterior = anterior->prox;
+                    seguinte = seguinte->prox;
+                    qtdAcessos+=2;
+                }
+                DoubleNode* novo = new DoubleNode;
+                novo->valor = valor;
+                seguinte->antec = novo;
+                anterior->prox = novo;
+                novo->antec = anterior;
+                novo->prox = seguinte;
+                qtdAcessos+=4;
                 tamanho++;
             }
         }
@@ -69,6 +117,8 @@ int main(){
     l.armazenaValores(45);
     l.armazenaValores(100);
     l.armazenaValores(147);
+    l.inserePosicao(10,2);
+
     cout << l.tamanho << endl;
     l.imprime();
     
