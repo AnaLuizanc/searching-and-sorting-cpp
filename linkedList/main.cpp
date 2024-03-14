939c176b451d737d821319bb6e2c8c7acb143a89
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-13 22:24:10 -0300

    clean main function

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -9,23 +9,7 @@ using namespace std;
 
 
 int main(){
-    DoubleList l;
-    l.armazenaValores(12);
-    l.armazenaValores(45);
-    l.armazenaValores(100);
-    l.armazenaValores(147);
-    l.inserePosicao(10,2);
-    l.imprime();
-    l.swap(0,4);
-
-    cout << l.tamanho << endl;
-    l.imprime();
-    l.imprimeInverso();
-
-    cout << l.buscaValor(45) << l.buscaValor(24) << endl;
-
-    DoubleNode* menor = l.buscaMenor();
-    cout << "Menor: " << menor->valor << endl;
+    
     
     return 0;  
 }
