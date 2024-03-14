1d0240eb6e1700b67a088c62f5b755c5a72f972b
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-13 21:28:43 -0300

    change armazenaValores function

--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -28,6 +28,29 @@ class DoubleList{
         DoubleNode* cabeca = new DoubleNode;
         DoubleNode* cauda = new DoubleNode;
 
+        void armazenaValores(int valor){
+            if(tamanho == 0){
+                DoubleNode* novo = new DoubleNode;
+                cabeca->prox = novo;
+                cauda->antec = novo;
+                novo->valor = valor;
+                novo->prox = cauda;
+                novo->antec = cabeca;
+                qtdAcessos+=2;
+                tamanho++;
+            }else{
+                DoubleNode* ultimo = cauda->antec;
+                DoubleNode* novo = new DoubleNode;
+                novo->valor = valor;
+                novo->prox = cauda;
+                novo->antec = ultimo;
+                cauda->antec = novo;
+                ultimo->prox = novo;
+                qtdAcessos+=5;
+                tamanho++;
+            }
+        }
+
         
 };
 
