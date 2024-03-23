7f2affe972abb6c11f882a78be4b997c98a04a4a
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-23 16:34:01 -0300

    create InsertionSort class

--- insertionSelectionSort/sort.h
+++ insertionSelectionSort/sort.h
@@ -1,6 +1,17 @@
 #ifndef SORT_H_INCLUDED
 #define SORT_H_INCLUDED
 
+class InsertionSort{
+    public:
+        InsertionSort(){
+            qtdAcessos = 0;
+            tamanho = 0;
+        }
+        ~InsertionSort(){}
 
+        int vetor[N_MAX];
+        unsigned qtdAcessos;
+        unsigned tamanho;
+}
 
 #endif
