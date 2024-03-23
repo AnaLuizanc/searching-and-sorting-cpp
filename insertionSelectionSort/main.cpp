1380e9e52d325736507e9f70114b4c200b3a899c
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-23 17:37:12 -0300

    create main function

--- insertionSelectionSort/main.cpp
+++ insertionSelectionSort/main.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 using namespace std;
+#define N_MAX 20
 #include"sort.h"
 
 int main(){
+    InsertionSort l;
+    int vetor[N_MAX] = {70,1,12,8,99,72,5,15,20,91,14,61,66,41,81,88,16,21,34,90};
+    l.armazenaValores(vetor);
+    l.ordena();
 
     return 0;
 }
