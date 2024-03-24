495546f4ad07df12c3399e8af85bfad27f2f8c78
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-03-23 21:45:50 -0300

    change main function

--- insertionSelectionSort/main.cpp
+++ insertionSelectionSort/main.cpp
@@ -8,6 +8,7 @@ int main(){
     int vetor[N_MAX] = {70,1,12,8,99,72,5,15,20,91,14,61,66,41,81,88,16,21,34,90};
     l.armazenaValores(vetor);
     l.ordena();
+    cout << "Numero de acessos: " << l.qtdAcessos << endl;
 
     return 0;
 }
