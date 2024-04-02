6683d3f262636f72322417ad9efbf747ba98fb37
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-04-02 20:23:01 -0300

    change main function

--- insertionSelectionSort/main.cpp
+++ insertionSelectionSort/main.cpp
@@ -1,22 +1,26 @@
 #include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 #define N 20
 #define N_MAX 100
 #include"sort.h"
 
 int main(){
-    InsertionSort l;
-    int vetor[N_MAX] = {70,1,12,8,99,72,5,15,20,91,14,61,66,41,81,88,16,21,34,90};
-    cout << "INSERTION SORT" << endl;
-    l.armazenaValores(vetor);
-    l.ordena();
-    cout << "Numero de acessos: " << l.qtdAcessos << endl;
+    InsertionSort insert;
+    SelectionSort select;
+    int lista[N] = {70,1,12,8,99,72,5,15,20,91,14,61,66,41,81,88,16,21,34,90};
 
-    cout << "SELECTION SORT" << endl;
-    SelectionSort l2;
-    l2.armazenaValores(vetor);
-    l2.ordena();
-    cout << "Numero de acessos: " << l2.qtdAcessos << endl;
+    cout << "\t\tINSERTION SORT" << endl;
+    insert.ordena(lista);
+    cout << "Numero de acessos: " << insert.qtdAcessos << endl;
+    
+    cout << "\t\tSELECTION SORT" << endl;
+    select.ordena(lista);
+    cout << "Numero de acessos: " << select.qtdAcessos << endl;
+
+
 
     return 0;
 }
