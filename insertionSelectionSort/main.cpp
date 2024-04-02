0e20639e2cefa86672756b7f5af01cf86e79a5a9
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-04-02 20:35:18 -0300

    change main function

--- insertionSelectionSort/main.cpp
+++ insertionSelectionSort/main.cpp
@@ -10,15 +10,17 @@ using namespace std;
 int main(){
     InsertionSort insert;
     SelectionSort select;
-    int lista[N] = {70,1,12,8,99,72,5,15,20,91,14,61,66,41,81,88,16,21,34,90};
+    int lista[N_MAX] = {70,1,12,8,99,72,5,15,20,91,14,61,66,41,81,88,16,21,34,90};
 
     cout << "\t\tINSERTION SORT" << endl;
-    insert.ordena(lista, 20);
+    insert.ordena(lista, N);
     cout << "Numero de acessos: " << insert.qtdAcessos << endl;
     
     cout << "\t\tSELECTION SORT" << endl;
-    select.ordena(lista, 20);
+    select.ordena(lista, N);
     cout << "Numero de acessos: " << select.qtdAcessos << endl;
+
+    //system("clear");
 
 
     return 0;
