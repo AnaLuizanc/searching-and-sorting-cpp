ce62daead9c84181f4d3bea424acc271f386b6a8
AnaLuizanc <99134340+AnaLuizanc@users.noreply.github.com>
2024-04-02 22:06:43 -0300

    create calls in the main function

--- insertionSelectionSort/main.cpp
+++ insertionSelectionSort/main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main(){
     InsertionSort insert;
     SelectionSort select;
-    int lista[N_MAX] = {70,1,12,8,99,72,5,15,20,91,14,61,66,41,81,88,16,21,34,90};
+    int lista[N] = {70,1,12,8,99,72,5,15,20,91,14,61,66,41,81,88,16,21,34,90};
 
     cout << "\t\tINSERTION SORT" << endl;
     insert.ordena(lista, N);
@@ -20,8 +20,28 @@ int main(){
     select.ordena(lista, N);
     cout << "Numero de acessos: " << select.qtdAcessos << endl;
 
-    //system("clear");
+    system("clear");
 
+    FILE* arq;
+    arq = fopen("geraGrafico.txt", "w");
+    int listaAleatoria[N_MAX];
+    unsigned tamanho;
+    srand(time(0));
+    // EXECUTA 100 VEZES
+    for(int i = 0; i<N_MAX; i++){
+        insert.tamanho = 0;
+        insert.qtdAcessos = 0;
+        select.tamanho = 0;
+        select.qtdAcessos = 0;
+        tamanho = rand() % 100 + 1;
+        
+        for(int j=0; j<tamanho; j++)
+            listaAleatoria[j] = rand() % 1000 + 1;
+        insert.ordenaListaAleatoria(listaAleatoria,tamanho);
+        select.ordenaListaAleatoria(listaAleatoria,tamanho);
+        cout << i+1 << "\t" << insert.tamanho << "\t" << insert.qtdAcessos << "\t" << select.qtdAcessos << endl;
+        //insert.imprime();
+    }
 
     return 0;
 }
